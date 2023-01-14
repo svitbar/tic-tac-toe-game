8ca5add82d6eb6ed6ffb1745f2657f854eb7b810
svitbar <svitbar1102@gmail.com>
2023-01-14 16:03:13 +0200

    Added dependencies files and configuration that started server

---
+++ registration/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello</h1>');
+});
+
+app.listen(5000, () => {
+  console.log('Sever started');
+});
---
+++ registration/sql/script.sql
@@ -0,0 +1,31 @@
+-- -- MySQL Workbench Forward Engineering
+
+-- SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+-- SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+-- SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -- -----------------------------------------------------
+-- -- Schema register_db
+-- -- -----------------------------------------------------
+
+-- -- -----------------------------------------------------
+-- -- Schema register_db
+-- -- -----------------------------------------------------
+-- CREATE SCHEMA IF NOT EXISTS `register_db` DEFAULT CHARACTER SET utf8 ;
+-- USE `register_db` ;
+
+-- -- -----------------------------------------------------
+-- -- Table `register_db`.`user`
+-- -- -----------------------------------------------------
+-- CREATE TABLE IF NOT EXISTS `register_db`.`user` (
+--   `username` VARCHAR(45) NOT NULL,
+--   `email` VARCHAR(255) NULL,
+--   `password` VARCHAR(32) NOT NULL,
+--   `id` INT NOT NULL AUTO_INCREMENT,
+--   PRIMARY KEY (`id`))
+-- ENGINE = InnoDB;
+
+
+-- SET SQL_MODE=@OLD_SQL_MODE;
+-- SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+-- SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
